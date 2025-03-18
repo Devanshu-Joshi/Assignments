1b45958edb813e57647c1c491d3d255be8c45f2e
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-03-18 23:00:41 +0530

    Design Done

--- JS_Assignment-3/script.js
+++ JS_Assignment-3/script.js
@@ -1,8 +1,13 @@
 let quote = null;
+let quoteTextHtml = document.querySelector(".quoteText");
 
 let quotePromise = fetch("https://api.freeapi.app/api/v1/public/quotes/quote/random");
 
 quotePromise.then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => data.data.content)
+    .then(quote => {
+        this.quote = quote;
+        quoteTextHtml.innerText = this.quote;
+        // document.write(this.quote);
+    })
     .catch(error => console.error(error));
-
