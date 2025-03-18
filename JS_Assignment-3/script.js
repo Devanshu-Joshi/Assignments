94fca7f62afd13fcf5e5236a5b551993b35b321b
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-03-18 23:39:12 +0530

    Cursor Effect Done

--- JS_Assignment-3/script.js
+++ JS_Assignment-3/script.js
@@ -1,6 +1,8 @@
 let quote = null;
 let quoteTextHtml = document.querySelector(".quoteText");
 let copyButton = document.querySelector(".copy-btn");
+let cursor = document.querySelector(".cursor");
+let allButtons = document.querySelectorAll(".btnAll");
 
 let quotePromise = fetch("https://api.freeapi.app/api/v1/public/quotes/quote/random");
 
@@ -20,3 +22,22 @@ copyButton.addEventListener("click", () => {
         copyButton.innerText = "COPY";
     }, 2000);
 })
+
+document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+})
+
+allButtons.forEach(btn => {
+    btn.addEventListener("mouseenter", () => {
+        cursor.style.width = "75px";
+        cursor.style.height = "75px";
+        cursor.style.border = "2px solid #fff";
+    });
+
+    btn.addEventListener("mouseleave", () => {
+        cursor.style.width = "20px";
+        cursor.style.height = "20px";
+        cursor.style.border = "10px solid #fff";
+    });
+});
