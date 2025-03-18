73a56d8dd0236415dee8c53b5136d0552fd85b73
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-03-18 23:11:21 +0530

    Basic JS Logic Done

--- JS_Assignment-3/script.js
+++ JS_Assignment-3/script.js
@@ -1,5 +1,6 @@
 let quote = null;
 let quoteTextHtml = document.querySelector(".quoteText");
+let copyButton = document.querySelector(".copy-btn");
 
 let quotePromise = fetch("https://api.freeapi.app/api/v1/public/quotes/quote/random");
 
@@ -11,3 +12,11 @@ quotePromise.then(res => res.json())
         // document.write(this.quote);
     })
     .catch(error => console.error(error));
+
+copyButton.addEventListener("click", () => {
+    navigator.clipboard.writeText(this.quote);
+    copyButton.innerText = "COPIED";
+    setTimeout(() => {
+        copyButton.innerText = "COPY";
+    }, 2000);
+})
