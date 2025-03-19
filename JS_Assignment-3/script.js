7151e631101d12e75f02fcb33f48b4619a276d97
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-03-19 12:01:28 +0530

    Twitter and Author Showing Done

--- JS_Assignment-3/script.js
+++ JS_Assignment-3/script.js
@@ -1,19 +1,31 @@
 let quote = null;
+let author = null;
 let quoteTextHtml = document.querySelector(".quoteText");
+let authorTextHtml = document.querySelector(".quoteAuthor");
 let copyButton = document.querySelector(".copy-btn");
+let refreshButton = document.querySelector(".btnRefresh");
 let cursor = document.querySelector(".cursor");
 let allButtons = document.querySelectorAll(".btnAll");
+let btnTwitter = document.querySelector(".btnTwitter");
 
-let quotePromise = fetch("https://api.freeapi.app/api/v1/public/quotes/quote/random");
+function fetchQuote() {
+    quoteTextHtml.innerText = "Quote is loading...";
+    authorTextHtml.innerText = "- Author";
 
-quotePromise.then(res => res.json())
-    .then(data => data.data.content)
-    .then(quote => {
-        this.quote = quote;
-        quoteTextHtml.innerText = this.quote;
-        // document.write(this.quote);
-    })
-    .catch(error => console.error(error));
+    let quotePromise = fetch("https://api.freeapi.app/api/v1/public/quotes/quote/random");
+
+    quotePromise.then(res => res.json())
+        .then(data => data.data)
+        .then(dataContent => {
+            console.log(dataContent);
+            this.quote = dataContent.content;
+            this.author = dataContent.author;
+            quoteTextHtml.innerText = this.quote;
+            authorTextHtml.innerText = "- " + this.author;
+        })
+        .catch(error => console.error(error));
+
+}
 
 copyButton.addEventListener("click", () => {
     navigator.clipboard.writeText(this.quote);
@@ -23,21 +35,31 @@ copyButton.addEventListener("click", () => {
     }, 2000);
 })
 
-document.addEventListener("mousemove", (e) => {
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
-})
-
-allButtons.forEach(btn => {
-    btn.addEventListener("mouseenter", () => {
-        cursor.style.width = "75px";
-        cursor.style.height = "75px";
-        cursor.style.border = "2px solid #fff";
-    });
-
-    btn.addEventListener("mouseleave", () => {
-        cursor.style.width = "20px";
-        cursor.style.height = "20px";
-        cursor.style.border = "10px solid #fff";
-    });
+btnTwitter.addEventListener("click", () => {
+    window.open("https://twitter.com/intent/tweet?text=" + this.quote + " - " + this.author, "_blank");
 });
+
+fetchQuote();
+
+refreshButton.addEventListener("click", () => { fetchQuote() });
+
+if (window.innerWidth > 600) {
+    document.addEventListener("mousemove", (e) => {
+        cursor.style.left = e.pageX + 'px';
+        cursor.style.top = e.pageY + 'px';
+    })
+
+    allButtons.forEach(btn => {
+        btn.addEventListener("mouseenter", () => {
+            cursor.style.width = "75px";
+            cursor.style.height = "75px";
+            cursor.style.border = "2px solid #fff";
+        });
+
+        btn.addEventListener("mouseleave", () => {
+            cursor.style.width = "20px";
+            cursor.style.height = "20px";
+            cursor.style.border = "10px solid #fff";
+        });
+    });
+}
